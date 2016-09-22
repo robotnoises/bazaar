6a1b26f53de89680fe0b84d5ac50ebe386e4b3d0
David Nichols <davenich@gmail.com>
2016-09-21 20:36:15 -0400

    feat(models): Add Role Model

--- api/models/Role.js
+++ api/models/Role.js
@@ -1,1 +1,34 @@
-// todo: User roles
+'use strict';
+
+var Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  
+  var Role = sequelize.define('role', {
+    type: { 
+      type: Sequelize.STRING
+    }
+  });
+
+  return Role.sync({force: true})
+    .then(() => {
+      return Role.create({
+        type: 'user'
+      });
+    })
+    .then(() => {
+      return Role.create({
+        type: 'admin'
+      })
+    })
+    .then(() => {
+      return Role.create({
+        type: 'superadmin'
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+
--- api/models/index.js
+++ api/models/index.js
@@ -2,17 +2,20 @@
 
 var User = require('./User');
 var Item = require('./Item');
+var Role = require('./Role');
 
 function init(sequelize) {
   return new Promise((resolve, reject) => {
+    
+    // Define and sync data models
+    
     User(sequelize)
-      .then(() => {
-        return Item(sequelize);
-      })
+      .then(() => Item(sequelize))
+      .then(() => Role(sequelize))
       .then(resolve)
       .catch((error) => {
         reject(error);
-      })
+      });
   });  
 }
 
